27aafc4b83e5ae6bab11b6fcfefa7f0e99a058ad
Alexandre Oliveira Queiroz <alezoq86@gmail.com>
2021-09-08 11:14:13 -0400

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/Services/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/Services/SellerService.java
@@ -1,11 +1,10 @@
-package com.devsuperior.dsvendas.Services;
+package com.devsuperior.dsvendas.services;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
@@ -19,5 +18,6 @@ public class SellerService {
 	public List<SellerDTO> findAll(){
 		List<Seller> result = repository.findAll();
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+				
 	}
 }
